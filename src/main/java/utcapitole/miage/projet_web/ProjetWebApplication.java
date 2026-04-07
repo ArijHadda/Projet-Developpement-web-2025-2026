f3353757ad94b531afdfb21eb06da1600f86e3a6
arijh <arijh43@gmail.com>
2026-04-07 13:23:34 +0200

    ajout du projet Springboot

---
+++ src/main/java/utcapitole/miage/projet_web/ProjetWebApplication.java
@@ -0,0 +1,13 @@
+package utcapitole.miage.projet_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetWebApplication.class, args);
+	}
+
+}
