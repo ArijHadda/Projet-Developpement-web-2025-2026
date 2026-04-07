9f35d453d59036e73d4377bdf06f09f8609988b3
arijh <arijh43@gmail.com>
2026-04-07 17:04:33 +0200

    ajout des attributs d'utilisateur

--- src/main/java/utcapitole/miage/projet_web/model/Utilisateur.java
+++ src/main/java/utcapitole/miage/projet_web/model/Utilisateur.java
@@ -1,7 +1,26 @@
 package utcapitole.miage.projet_web.model;
 
-import jakarta.persistence.Entity;
-
+import jakarta.persistence.*;
+import lombok.*;
 @Entity
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Utilisateur")
 public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long IdU;
+    private String nomU;
+    private String prenomU;
+    private String mailU;
+    private String mdpU;
+    private String sexeU;
+    private int ageU;
+    private float tailleU;
+    private float poidsU;
+
+
 }
